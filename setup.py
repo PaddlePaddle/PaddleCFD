b6b33636b63aca835a97164e5645c207f5f9ad83
KaiCHEN-HT <kaich_xjtu@163.com>
2025-05-08 03:20:25 +0000

    pip package build

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         description='PaddleCFD is a deep learning toolkit for surrogate modeling, equation discovery, shape optimization and flow-control strategy discovery in the field of fluid mechanics.',
         long_description=open('README.md', encoding="utf-8").read(),
         long_description_content_type='text/markdown',
-        author='PaddlePaddle AI4S',
+        author='AI4S@PaddlePaddle',
         author_email='paddlescience@baidu.com',
         url='https://github.com/PaddlePaddle/PaddleCFD',
         install_requires=get_requirements(),
