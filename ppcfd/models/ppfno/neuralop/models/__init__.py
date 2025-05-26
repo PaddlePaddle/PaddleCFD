d971e6b53ad6f8befc6b6ee5c4a99d5733656118
KaiCHEN-HT <kaich_xjtu@163.com>
2025-05-26 13:13:16 +0000

    complete work flow, lower mre ppfno model

--- ppcfd/models/ppfno/neuralop/models/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-from .fno import TFNO, TFNO1d, TFNO2d, TFNO3d
-from .fno import FNO, FNO1d, FNO2d, FNO3d
-from .base_model import get_model
---
+++ ppcfd/models/ppfno/src/neuralop/models/__init__.py
@@ -0,0 +1,11 @@
+import sys
+
+from .base_model import get_model
+from .fno import FNO
+from .fno import TFNO
+from .fno import FNO1d
+from .fno import FNO2d
+from .fno import FNO3d
+from .fno import TFNO1d
+from .fno import TFNO2d
+from .fno import TFNO3d
