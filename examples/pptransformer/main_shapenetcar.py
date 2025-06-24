3e596651243deb1932a58c5c01ead5a3f33e7669
WG <39621324+wangguan1995@users.noreply.github.com>
2025-06-24 11:23:19 +0800

    Update main_shapenetcar.py

--- examples/pptransformer/main_shapenetcar.py
+++ examples/pptransformer/main_shapenetcar.py
@@ -1,4 +1,17 @@
-import os
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import time
 import scipy
@@ -12,20 +25,47 @@ from datetime import datetime
 from paddle.io import DataLoader
 from ppcfd.data import GraphDataset
 from sklearn.isotonic import spearmanr
-from drag_coefficient import cal_coefficient
+# from drag_coefficient import cal_coefficient
 from ppcfd.networks.Transolver_orig import Model
 from ppcfd.utils.profiler import init_profiler, update_profiler
 from ppcfd.data.shapenetcar_datamodule import load_train_val_fold
 
-paddle.seed(1024)
-np.random.seed(1024)
+paddle.seed(42)
+np.random.seed(42)
 
 def get_nb_trainable_params(model):
+    """
+    获取模型中的可训练参数数量。
+
+    Args:
+        model (torch.nn.Module): 需要计算可训练参数数量的模型。
+
+    Returns:
+        int: 模型中的可训练参数数量。
+
+    """
     model_parameters = filter(lambda p: not p.stop_gradient, model.parameters())
     return sum([np.prod(tuple(p.shape)) for p in model_parameters])
 
 
 def train(model, train_loader, optimizer, scheduler, reg=1, epoch=0, prof=None):
+    """
+    当前epoch训练模型
+
+    Args:
+        model (paddle.nn.Layer): 模型
+        train_loader (paddle.io.DataLoader): 训练数据加载器
+        optimizer (paddle.optimizer.Optimizer): 优化器
+        scheduler (paddle.optimizer.lr.LearningRateScheduler): 学习率调度器
+        reg (float, optional): 正则化项系数，默认为1
+        epoch (int, optional): 当前训练轮数，默认为0
+        prof (Optional[dict], optional): 用于性能分析的字典，默认为None
+
+    Returns:
+        tuple: 包含平均压力损失和平均速度损失的元组
+
+    """
+    enable_prof = True if prof is not None else False
     model.train()
     criterion_func = paddle.nn.MSELoss(reduction="none")
     losses_press = []
@@ -44,7 +84,7 @@ def train(model, train_loader, optimizer, scheduler, reg=1, epoch=0, prof=None):
         total_loss.backward()
         optimizer.step()
         scheduler.step()
-        prof = update_profiler(False, prof, epoch)
+        prof = update_profiler(enable_prof, prof, epoch)
         losses_press.append(loss_press.item())
         losses_velo.append(loss_velo.item())
         losses_press.append(total_loss.item())
@@ -54,6 +94,25 @@ def train(model, train_loader, optimizer, scheduler, reg=1, epoch=0, prof=None):
 
 @paddle.no_grad()
 def test(model, test_loader, coef_norm, enable_test=False):
+    """
+    测试模型。
+
+    Args:
+        model (paddle.nn.Layer): 需要测试的模型。
+        test_loader (paddle.io.DataLoader): 测试数据集加载器。
+        coef_norm (list): 归一化系数列表。
+        enable_test (bool, optional): 是否启用详细测试输出。默认为 False。
+
+    Returns:
+        tuple: 包含以下元素的元组:
+            - mean_loss_press (float): 压力损失的均值。
+            - mean_loss_velo (float): 速度损失的均值。
+            - mean_loss_press_orig (float): 原始压力损失的均值。
+            - mean_loss_velo_orig (float): 原始速度损失的均值。
+            - spear (float): Spearman 相关系数。
+            - cd_mre (float): 平均相对误差。
+
+    """
     model.eval()
     criterion_func = paddle.nn.MSELoss(reduction="none")
     losses_press, losses_velo, losses_cd, p_orig, v_orig = [], [], [], [], []
@@ -66,7 +125,6 @@ def test(model, test_loader, coef_norm, enable_test=False):
         p_true = paddle.stack([targets[j][surf[j], -1:] for j in range(B)], axis=0)
 
         eps = 1e-8
-        # inputs_orig = (inputs * (coef_norm[1] + eps) + coef_norm[0])
         out_orig = (out * (coef_norm[3] + eps) + coef_norm[2])
         targets_orig = (targets *(coef_norm[3] + eps) + coef_norm[2])
         p_pred_orig = paddle.stack([out_orig[j][surf[j], -1:] for j in range(B)], axis=0)
@@ -89,6 +147,7 @@ def test(model, test_loader, coef_norm, enable_test=False):
             # print(f"{i} {sample_name[0]}, loss_velo = {loss_velo_orig.item():.4f}, loss_press = {loss_press_orig.item():.4f}, loss_cd = {loss_cd.item():.4f}")
 
         # import meshio
+        # inputs_orig = (inputs * (coef_norm[1] + eps) + coef_norm[0])
         # centroid = paddle.stack([inputs_orig[j][surf[j], :3] for j in range(B)], axis=0)
         # for j in range(B):
         #     cells = [('vertex', np.arange(tuple(centroid[j].shape)[0]).reshape(-1, 1))]
@@ -122,7 +181,18 @@ def test(model, test_loader, coef_norm, enable_test=False):
 
 
 def save_checkpoint(path, epoch, model):
-    model_path = os.path.join(path, f"model_{epoch+1}.pdparams")
+    """
+    保存模型检查点。
+
+    Args:
+        path (str): 保存检查点的路径。
+        epoch (int): 当前训练的轮数。
+        model (paddle.Model): 要保存的检查点模型。
+
+    Returns:
+        None
+    """
+    model_path = os.path.join(path, f"model_{epoch}.pdparams")
     state_dict = {k: v.cpu() for k, v in model.state_dict().items()}
     paddle.save(state_dict, model_path)
 
@@ -136,8 +206,28 @@ def main(
     reg=1,
     val_iter=1,
     coef_norm=[],
-    enable_test=False
+    enable_test=False,
+    enable_prof=False
 ):
+    """
+    训练深度学习模型。
+
+    Args:
+        train_dataset (Dataset): 训练数据集。
+        val_dataset (Dataset): 验证数据集。
+        model (Model): 待训练的深度学习模型。
+        hparams (dict): 包含训练参数的超参数字典，如学习率（lr）、批量大小（batch_size）和训练轮数（nb_epochs）等。
+        path (str): 模型和训练结果的保存路径。
+        reg (float, optional): 正则化系数，默认为1。
+        val_iter (int, optional): 验证间隔，即每训练多少个epoch验证一次，默认为1。
+        coef_norm (list, optional): 系数归一化列表，默认为空列表。
+        enable_test (bool, optional): 是否启用测试模式，默认为False。
+        enable_prof (bool, optional): 是否启用性能分析，默认为False。
+
+    Returns:
+        Model: 训练后的模型。
+
+    """
     optimizer = paddle.optimizer.Adam(
         parameters=model.parameters(), learning_rate=hparams["lr"], weight_decay=0.0
     )
@@ -150,7 +240,7 @@ def main(
 
     start = time.time()
     train_loss, val_loss = 100000.0, 100000.0
-    prof = init_profiler(False)
+    prof = init_profiler(enable_prof)
     custom_collate_fn = None
     train_loss_list, val_loss_list = [], []
     val_loader = DataLoader(val_dataset, batch_size=1, collate_fn=custom_collate_fn)
@@ -163,7 +253,8 @@ def main(
     )
 
     if enable_test:
-        state_dict = paddle.load("./checkpoints/Transolver_ShapeNetCar_120.pdparams")
+        state_dict = paddle.load("./model_131.pdparams")
+        # val_loss = 0.0619, Spearman's Rank Correlations = 0.0000, loss_velo = 0.0238, loss_press = 0.0760, loss_cd = 0.0000
         model.set_state_dict(state_dict)
         loss_press, loss_velo, p_orig, v_orig, spearmanr, loss_cd = test(model, val_loader, coef_norm, enable_test=True)
         print(f"val_loss = {(loss_press + reg*loss_velo):.4f}, Spearman's Rank Correlations = {spearmanr:.4f}, loss_velo = {v_orig.item():.4f}, loss_press = {p_orig.item():.4f}, loss_cd = {loss_cd:.4f}")
@@ -192,68 +283,69 @@ def main(
     return model
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--data_dir", default="./data/training_data")
-parser.add_argument("--save_dir", default="./data/preprocessed_data")
-parser.add_argument("--fold_id", default=0, type=int)
-parser.add_argument("--gpu", default=0, type=int)
-parser.add_argument("--val_iter", default=10, type=int)
-parser.add_argument("--cfd_config_dir", default="cfd/cfd_params.yaml")
-parser.add_argument("--cfd_model", default="Transolver")
-parser.add_argument("--cfd_mesh", default=False)
-parser.add_argument("--enable_test", default=False)
-parser.add_argument("--r", default=0.2, type=float)
-parser.add_argument("--weight", default=0.5, type=float)
-parser.add_argument("--lr", default=0.001, type=float)
-parser.add_argument("--batch_size", default=1, type=int)
-parser.add_argument("--nb_epochs", default=155, type=int)
-parser.add_argument("--preprocessed", default=1, type=int)
-parser.add_argument("--cinn", default=False, type=bool)
-parser.add_argument("--n_train", default=800, type=int)
-parser.add_argument("--n_eval", default=200, type=int)
-args = parser.parse_args()
-print(args)
-hparams = {"lr": args.lr, "batch_size": args.batch_size, "nb_epochs": args.nb_epochs}
-train_data, val_data, coef_norm = load_train_val_fold(
-    args, preprocessed=args.preprocessed
-)
-train_ds = GraphDataset(train_data, use_cfd_mesh=args.cfd_mesh, r=args.r)
-val_ds = GraphDataset(val_data, use_cfd_mesh=args.cfd_mesh, r=args.r)
-
-if args.cfd_model == "Transolver":
-    model = Model(
-        n_hidden=256,
-        n_layers=8,
-        space_dim=7,
-        fun_dim=0,
-        n_head=8,
-        mlp_ratio=2,
-        out_dim=4,
-        slice_num=32,
-        unified_pos=False,
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", default="../data/shapenetcar/training_data")
+    parser.add_argument("--save_dir", default="../data/shapenetcar/preprocessed_data")
+    parser.add_argument("--fold_id", default=0, type=int)
+    parser.add_argument("--val_iter", default=10, type=int)
+    parser.add_argument("--cfd_model", default="Transolver")
+    parser.add_argument("--cfd_mesh", default=False)
+    parser.add_argument("--enable_test", default=False)
+    parser.add_argument("--r", default=0.2, type=float)
+    parser.add_argument("--weight", default=0.5, type=float)
+    parser.add_argument("--lr", default=0.001, type=float)
+    parser.add_argument("--batch_size", default=1, type=int)
+    parser.add_argument("--nb_epochs", default=200, type=int)
+    parser.add_argument("--preprocessed", default=1, type=int)
+    parser.add_argument("--cinn", default=False, type=bool)
+    parser.add_argument("--profiler", default=False, type=bool)
+    parser.add_argument("--n_train", default=800, type=int)
+    parser.add_argument("--n_eval", default=200, type=int)
+    args = parser.parse_args()
+    print(args)
+    hparams = {"lr": args.lr, "batch_size": args.batch_size, "nb_epochs": args.nb_epochs}
+    train_data, val_data, coef_norm = load_train_val_fold(
+        args, preprocessed=args.preprocessed
     )
-    
-    if args.cinn:
-        import einops
-        paddle.jit.ignore_module([einops])
-        paddle.framework.core._set_prim_all_enabled(True)
-        model = paddle.jit.to_static(model, full_graph=True, backend='CINN', input_spec = [paddle.static.InputSpec(shape=[1, None, 7], dtype='float32')])
-else:
-    raise NotImplementedError
+    train_ds = GraphDataset(train_data, use_cfd_mesh=args.cfd_mesh, r=args.r)
+    val_ds = GraphDataset(val_data, use_cfd_mesh=args.cfd_mesh, r=args.r)
 
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-path = f"metrics/{args.cfd_model}/{timestamp}"
-if not os.path.exists(path):
-    os.makedirs(path)
+    if args.cfd_model == "Transolver":
+        model = Model(
+            n_hidden=256,
+            n_layers=8,
+            space_dim=7,
+            fun_dim=0,
+            n_head=8,
+            mlp_ratio=2,
+            out_dim=4,
+            slice_num=32,
+            unified_pos=False,
+        )
+        
+        if args.cinn:
+            import einops
+            paddle.jit.ignore_module([einops])
+            paddle.framework.core._set_prim_all_enabled(True)
+            model = paddle.jit.to_static(model, full_graph=True, input_spec = [paddle.static.InputSpec(shape=[32186, 7], dtype='float32')])
+    else:
+        raise NotImplementedError
 
-model = main(
-    train_ds,
-    val_ds,
-    model,
-    hparams,
-    path,
-    val_iter=args.val_iter,
-    reg=args.weight,
-    coef_norm=coef_norm,
-    enable_test=args.enable_test
-)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    path = f"metrics/{args.cfd_model}/{timestamp}"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    model = main(
+        train_ds,
+        val_ds,
+        model,
+        hparams,
+        path,
+        val_iter=args.val_iter,
+        reg=args.weight,
+        coef_norm=coef_norm,
+        enable_test=args.enable_test,
+        enable_prof=args.profiler
+    )
