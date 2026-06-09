18a55f371bff4d9c86e6dfdfd5be0930a4074ba8
guhexiang <1121378443@qq.com>
2026-06-09 21:30:43 +0800

    fix UPT bug

---
+++ source/UPT/kappaconfig/entities/__init__.py
@@ -0,0 +1,3 @@
+from .wrappers import KCScalar
+
+__all__ = ["KCScalar"]
---
+++ source/UPT/kappaconfig/entities/wrappers.py
@@ -0,0 +1,6 @@
+class KCScalar:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f"KCScalar({self.value!r})"
